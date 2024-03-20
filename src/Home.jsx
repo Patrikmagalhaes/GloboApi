eb1813a06f7b93002aa84533c02e9cf9b3dd3c7c
Patrikmagalhaes <patrikmagalhaes@icloud.com>
2024-03-20 15:29:10 -0300

    exibindo dados da api

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,40 +1,40 @@
 import { useState, useEffect } from "react"
 
 function Home() {
-    const [pesquisa, setPesquisa] = useState("Bat")
-    const [filmes, setFilmes] = useState()
+    const [pesquisa, setPesquisa] = useState("Batman")
+    const [filmes, setFilmes] = useState(null)
 
     useEffect(() => {
         fetch(`https://www.omdbapi.com/?apikey=28d0dee8&type=movie&s=${pesquisa}`)
-
             .then(response => response.json())
             .then(data => {
-                setFilmes(data)
+                if (data.Response == 'False') {
+                    console.log(data)
+                }else{
+                    setFilmes(data)
+                }
             })
-
-
 
     }, [pesquisa])
 
     const pesquisarItens = (valorPesquisa) => {
-        setPesquisa(valorPesquisa)
+
+        const valorInput = valorPesquisa.split("")
+        if (valorInput.length > 2) return setPesquisa(valorPesquisa)
+
     }
-    if (!filmes) {
-        "Carregando"
-    }
-    else {
+
+ if(!filmes)return"Carregando"
         return (
             <>
                 <div>
-                    {filmes.Search.map((item) => <li>{item.Title}</li>)}
-                    <h1>Ola </h1>
-                    <input onChange={(e) => pesquisarItens(e.target.value)} />
-                    <button ></button>
+                <h1>Discolândia </h1>
+                <input onChange={(e) => pesquisarItens(e.target.value)} />
+                    <button>Buscar</button>
+                   <ul>{filmes.Search.map((item) => <li>{item.Title} < img src={item.Poster}/></li>)}</ul>
                     <h2></h2>
                 </div>
             </>
         )
     }
-}
-
 export default Home
