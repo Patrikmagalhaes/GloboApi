ba0062d1ccb28193f1cf86f7c0bb5b034fd6513d
PATRIK DA SILVEIRA MAGALHÃES <patrik_magalhaes@estudante.sesisenai.org.br>
2024-03-20 19:57:02 -0300

    exibindo dados da api tela detalhes

--- src/Detalhes.jsx
+++ src/Detalhes.jsx
@@ -1,10 +1,28 @@
+import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
 
 function Detalhes(){
     const {imdbID} = useParams()
+const [filme, setFilme] = useState(null)
+
+    useEffect(() => {
+        fetch(`https://www.omdbapi.com/?apikey=28d0dee8&type=movie&i=${imdbID}`)
+            .then(response => response.json())
+            .then(data => {
+                if (data.Response == 'False') {
+                    console.log(data)
+                } else {
+                    setFilme(data)
+                }
+            })
+
+    }, [filme])
+
+    if(!filme)return"Carregando"
     return(
         <>
-        <h1>Opa</h1>
+     <h1>{filme.Title}</h1>
+     <img src={filme.Poster}/>
         </>
     )
 }
